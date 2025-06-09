c23cea01b555bf5c1c94217088134211e952696a
attila <116733342+1attila@users.noreply.github.com>
2025-06-09 22:07:18 +0200

    Added source files

---
+++ QSWE-V3 Schematic Maker/gui.py
@@ -0,0 +1,189 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QMessageBox
+from PyQt6.QtGui import QIntValidator
+
+import sys
+
+from schem_gen import QsweMaker
+
+
+class QsweGUI(QWidget):
+    """
+    QsweMaker GUI
+    """
+
+    qswe_maker: QsweMaker
+    input_x: QLineEdit
+    input_z: QLineEdit
+    input_start: QLineEdit
+    input_end: QLineEdit
+    label_result: QLabel
+    label_height: QLabel
+    label_status: QLabel
+
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.setWindowTitle("QSWE Schematic Maker")
+
+        self.qswe_maker = QsweMaker()
+
+        self.input_x = QLineEdit()
+        self.input_z = QLineEdit()
+        self.input_start = QLineEdit()
+        self.input_end = QLineEdit("-59")
+
+        int_validator = QIntValidator()
+        self.input_x.setValidator(int_validator)
+        self.input_z.setValidator(int_validator)
+        self.input_start.setValidator(int_validator)
+        self.input_end.setValidator(int_validator)
+
+        self.input_x.setPlaceholderText("Enter X size")
+        self.input_z.setPlaceholderText("Enter Z size")
+        self.input_start.setPlaceholderText("Enter starting Y")
+
+        self.label_result = QLabel("")
+        self.label_height = QLabel("")
+        self.label_status = QLabel("")
+
+        btn_calc_sizes = QPushButton("Calculate Sizes")
+        btn_calc_height = QPushButton("Calculate Height")
+        btn_generate = QPushButton("Generate Schematic")
+
+        btn_calc_sizes.clicked.connect(self.calculate_sizes)
+        btn_calc_height.clicked.connect(self.calculate_height)
+        btn_generate.clicked.connect(self.generate_schematic)
+
+        layout = QGridLayout()
+
+        layout.addWidget(QLabel("X Size:"), 0, 0)
+        layout.addWidget(self.input_x,      0, 1)
+        layout.addWidget(QLabel("Z Size:"), 1, 0)
+        layout.addWidget(self.input_z,      1, 1)
+        layout.addWidget(btn_calc_sizes,    0, 2)
+        layout.addWidget(btn_generate,      1, 2)
+        layout.addWidget(self.label_result, 2, 0, 1, 3)
+
+        layout.addWidget(QLabel("Start Y:"), 3, 0)
+        layout.addWidget(self.input_start,   3, 1)
+        layout.addWidget(QLabel("End Y:"),   4, 0)
+        layout.addWidget(self.input_end,     4, 1)
+        layout.addWidget(btn_calc_height,    3, 2, 2, 1)
+        layout.addWidget(self.label_height,  5, 0, 1, 3)
+
+        layout.addWidget(self.label_status, 7, 0, 1 ,3)
+
+        self.setLayout(layout)
+    
+
+    def calculate_sizes(self):
+
+        try:
+            x = int(self.input_x.text().strip())
+            z = int(self.input_z.text().strip())
+
+            sx, sz = QsweMaker.calculate_sizes(x, z)
+            dx = sx - x
+            dz = sz - z
+            xs = str(sx)
+            zs = str(sz)
+
+            if dx > 0:
+                xs += f" (+{dx})"
+        
+            if dz > 0:
+                zs += f" (+{dz})"
+
+            msg = f"Suggested sizes: {xs}, {zs}"
+            self.label_result.setText(msg)
+
+            self.input_x.setText(str(sx))
+            self.input_z.setText(str(sz))
+
+        except ValueError:
+            self.error("Invalid size inputs")
+        
+
+    def calculate_height(self):
+
+        try:
+
+            start = int(self.input_start.text().strip())
+            end = int(self.input_end.text().strip())
+
+            y = QsweMaker.calculate_initial_height(start, end)
+
+            self.label_height.setText(f"Right Height: Y={y}")
+
+        except ValueError:
+            self.error("Invalid height inputs")
+        
+
+    def generate_schematic(self):
+
+        try:
+
+            x = int(self.input_x.text().strip())
+            z = int(self.input_z.text().strip())
+
+            rigth_x, rigth_z = QsweMaker.calculate_sizes(x, z)
+
+            if rigth_x != x or rigth_z != z:
+
+                dx = rigth_x - x
+                dz = rigth_z - z
+                xs = str(rigth_x)
+                zs = str(rigth_z)
+
+                if dx > 0:
+                    xs += f" (+{dx})"
+        
+                if dz > 0:
+                    zs += f" (+{dz})"
+
+                msg = QMessageBox(self)
+                msg.setIcon(QMessageBox.Icon.Question)
+
+                msg.setWindowTitle("Adjusted Sizes")
+                msg.setText(
+                    f"The sizes you entered were adjusted to:\n\n"
+                    f"  X: {x} --> {xs}\n"
+                    f"  Z: {z} --> {zs}\n\n"
+                    "Do you want to continue with these sizes?"
+                )
+
+                msg.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+                result = msg.exec()
+
+                if result != QMessageBox.StandardButton.Yes:
+                    self.label_status.setText("Cancelled.")
+                    return
+
+                else:
+                    x, z = rigth_x, rigth_z
+                
+
+            self.label_status.setText("Generating schematic...")
+            self.qswe_maker.generate_schematic(x, z)
+            self.label_status.setText(f"Saved: QSWE-V3 {x}x{z}.litematic")
+
+        except Exception as e:
+            self.error(f"Error: {e}")
+
+
+    def error(self, msg):
+        QMessageBox.critical(self, "Error", msg)
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    window = QsweGUI()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ QSWE-V3 Schematic Maker/schem_gen.py
@@ -0,0 +1,287 @@
+from typing import Tuple
+import argparse
+import sys
+import os
+
+from litemapy import Schematic, Region, BlockState
+
+
+MIN_SIZE_X = 18
+MIN_SIZE_Z = 29
+
+
+def resource_path(relative_path: str) -> str:
+    """Get absolute path to resource, works for PyInstaller"""
+
+    if getattr(sys, 'frozen', False):
+        return os.path.join(sys._MEIPASS, relative_path)
+    
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+def concatenate_regions(reg1: Region, reg2: Region, x: int=0, y: int=0, z: int=0) -> Region:
+    """
+    Utility method to paste reg2 into reg2 at the given coordinates.
+    Resizes the region if needed
+    """
+
+    w = max(reg2.width  + x, reg1.width)
+    h = max(reg2.height + y, reg1.height)
+    l = max(reg2.length + z, reg1.length)
+
+    if w != reg1.width or h != reg1.height or l != reg1.length:
+
+        reg = Region(reg1.x, reg1.y, reg1.z, w, h, l)
+    
+        for bx, by, bz in reg1.block_positions():
+            reg[bx, by, bz] = reg1[bx, by, bz]
+        
+    else:
+        reg = reg1
+
+    for bx, by, bz in reg2.block_positions():
+        reg[bx + x, by + y, bz + z] = reg2[bx, by, bz]
+
+    return reg
+
+
+class QsweMaker:
+    """
+    Tiles the schematic for the QSWE and calculates the rigth sizes and height
+    """
+
+    _main_logic: Region
+    _main_dupers_stack: Region
+    _main_sweepers_stack: Region
+    _main_sweepers_end: Region
+    _return_logic: Region
+    _return_dupers_stack: Region
+    _return_sweepers_stack: Region
+    _return_sweepers_end: Region
+
+
+    def __init__(self, schem_to_load: str="SM.litematic") -> "QsweMaker":
+
+        sm_litematic = Schematic.load(resource_path(schem_to_load))
+
+        self._main_logic = sm_litematic.regions["MainLogic"]
+        self._main_dupers_stack = sm_litematic.regions["MainDupersStack"]
+        self._main_sweepers_stack = sm_litematic.regions["MainSweepersStack"]
+        self._main_sweepers_end = sm_litematic.regions["MainSweepersEnd"]
+
+        self._return_logic = sm_litematic.regions["ReturnLogic"]
+        self._return_dupers_stack = sm_litematic.regions["ReturnDupersStack"]
+        self._return_sweepers_stack = sm_litematic.regions["ReturnSweepersStack"]
+        self._return_sweepers_end = sm_litematic.regions["ReturnSweepersEnd"]
+
+
+    @classmethod
+    def calculate_sizes(self, x: int, z: int) -> Tuple[int]:
+        """
+        Calculate the rigth sizes for the world eater
+        """
+
+        if x % 6 != 0:
+            x = 6 + (x // 6) * 6
+
+        return max(x, MIN_SIZE_X) , max(MIN_SIZE_Z, z)
+
+
+    @classmethod
+    def calculate_initial_height(self, start: int, end: int) -> int:
+        """
+        Given the max terrain height and the point where the WE should stop, calculates the exact WE y
+        """
+
+        return start + (start - end) % 4
+
+
+    def generate_trenches_outline(self, size_x: int, size_z: int) -> Region:
+        """
+        Builds the trenches outline with leaves
+        """
+
+        trenches_outline_region = Region(0, 0, 0, size_x, 1, size_z)
+
+        for x in range(size_x):
+            trenches_outline_region[x, 0, 0]          = BlockState("minecraft:spruce_leaves")
+            trenches_outline_region[x, 0, size_z - 1] = BlockState("minecraft:spruce_leaves")
+
+        for z in range(size_z):
+            trenches_outline_region[         0, 0, z] = BlockState("minecraft:spruce_leaves")
+            trenches_outline_region[size_x - 1, 0, z] = BlockState("minecraft:spruce_leaves")
+
+        for x in range(1, size_x - 2):
+            trenches_outline_region[x, 0, 1]          = BlockState("minecraft:oak_leaves")
+            trenches_outline_region[x, 0, size_z - 2] = BlockState("minecraft:oak_leaves")
+
+        for z in range(1, size_z - 2):
+            trenches_outline_region[         1, 0, z] = BlockState("minecraft:oak_leaves")
+            trenches_outline_region[size_x - 2, 0, z] = BlockState("minecraft:oak_leaves")
+    
+        for x in range(4, size_x - 4):
+            trenches_outline_region[x, 0, 13]          = BlockState("minecraft:spruce_leaves")
+            trenches_outline_region[x, 0, size_z - 14] = BlockState("minecraft:spruce_leaves")
+
+        for x in range(3, size_x - 3):
+            trenches_outline_region[x, 0, 12]          = BlockState("minecraft:oak_leaves")
+            trenches_outline_region[x, 0, size_z - 13] = BlockState("minecraft:oak_leaves")
+    
+        for z in range(14, size_z - 14):
+            trenches_outline_region[4,          0, z] = BlockState("minecraft:spruce_leaves")
+            trenches_outline_region[size_x - 5, 0, z] = BlockState("minecraft:spruce_leaves")
+
+        for z in range(13, size_z - 13):
+            trenches_outline_region[3,          0, z] = BlockState("minecraft:oak_leaves")
+            trenches_outline_region[size_x - 4, 0, z] = BlockState("minecraft:oak_leaves")
+
+        return trenches_outline_region
+
+
+    def generate_main_side(self, size_x: int) -> Region:
+        """
+        Generates the main side of the WE
+        """
+    
+        main_side = Region(1, 2, 1, size_x - 4, 88, 11)
+        main_side = concatenate_regions(main_side, self._main_logic, 0, 0, 0)
+
+        for x in range(9, size_x - 14, 6):
+            main_side = concatenate_regions(main_side, self._main_sweepers_stack, x, 1, 0)
+            main_side = concatenate_regions(main_side, self._main_dupers_stack, x, 82, 6)
+
+        main_side = concatenate_regions(main_side, self._main_sweepers_end, size_x - 11, 1, 0)
+        main_side = concatenate_regions(main_side, self._main_dupers_stack, size_x - 11, 82, 6)
+    
+        return main_side
+
+
+    def generate_return_side(self, size_x: int, size_z: int) -> Region:
+        """
+        Generates the return side of the WE
+        """
+
+        return_side = Region(1, 4, size_z - 17, size_x - 4, 84, 16)
+        return_side = concatenate_regions(return_side, self._return_logic, 0, 0, -1)
+
+        for x in range(9, size_x - 14, 6):
+            return_side = concatenate_regions(return_side, self._return_sweepers_stack, x, 0, -1)
+            return_side = concatenate_regions(return_side, self._return_dupers_stack, x, 81, 4)
+
+        return_side = concatenate_regions(return_side, self._return_sweepers_end, size_x - 11, 0, -1)
+        return_side = concatenate_regions(return_side, self._return_dupers_stack, size_x - 11, 81, 4)
+
+        return return_side
+
+
+    def generate_schematic(self, size_x: int, size_z: int) -> Schematic:
+        """
+        Generate a schematic of the WE with the given sizes
+        """
+
+        trenches_outline = self.generate_trenches_outline(size_x + 2, size_z + 2)
+        main_side = self.generate_main_side(size_x + 2)
+        return_side = self.generate_return_side(size_x + 2, size_z + 2)
+
+        schematic = Schematic(
+            name=f"QSWE-V3 {size_x}x{size_z}",
+            author="QSWE-MakerByAttila",
+            regions={
+                "Trenches": trenches_outline,
+                "Main": main_side,
+                "Return": return_side
+            }
+        )
+
+        schematic.save(f"QSWE-V3 {size_x}x{size_z}.litematic")
+
+
+def sizes(args):
+
+    x, z = QsweMaker.calculate_sizes(args.x, args.z)
+    dx = x - args.x
+    dz = z - args.z
+    xs = str(x)
+    zs = str(z)
+
+    if dx > 0:
+        xs += f" (+{dx})"
+        
+    if dz > 0:
+        zs += f" (+{dz})"
+
+    print(f"Suggested sizes: {xs}, {zs}")
+
+    inp = input(f"Want to generate a {x}x{z} WE schematic? (y/n) ") or "n"
+
+    if inp.strip().lower() == "y":
+        
+        schem(argparse.Namespace(x=x, z=z))
+
+
+def schem(args):
+
+    schem_maker = QsweMaker()
+
+    x, z = schem_maker.calculate_sizes(args.x, args.z)
+
+    if x != args.x or z != args.z:
+        
+        sizes(args)
+        return
+
+    print("Making the schematic...")
+    schem_maker.generate_schematic(x, z)
+    print("Done!")
+
+    inp = input(f"Want to know at what height place the WE? (y/n) ") or "n"
+
+    if inp.strip().lower() == "y":
+        
+        start = int(input("Start y: "))
+        end = int(input("End y: (default=-59) ") or -59)
+
+        args.start = start
+        args.end = end
+        height(args)
+
+
+def height(args):
+
+    y = QsweMaker.calculate_initial_height(args.start, args.end)
+
+    print(f"Right height: Y={y}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="QSWE Schematic Maker")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # sizes command
+    parser_sizes = subparsers.add_parser("sizes", help="Get valid WE sizes")
+    parser_sizes.add_argument("x", type=int, help="X (width) of the perimeter")
+    parser_sizes.add_argument("z", type=int, help="Z (length) of the perimeter")
+
+    # height command
+    parser_height = subparsers.add_parser("height", help="Suggest rigth build height")
+    parser_height.add_argument("start", type=int, help="Start YEnd location or target Y")
+    parser_height.add_argument("end", type=int, nargs="?", help="End location or target Y (defaults to -59)", default=-59)
+
+    # schem command
+    parser_schem = subparsers.add_parser("schem", help="Generate QSWE schematic")
+    parser_schem.add_argument("x", type=int, help="Width of the perimeter")
+    parser_schem.add_argument("z", type=int, help="Length of the perimeter")
+
+    args = parser.parse_args()
+    {
+        "sizes": sizes,
+        "height": height,
+        "schem": schem
+    }.get(
+        args.command, lambda a: parser.print_help()
+    )(args)
+
+
+if __name__ == "__main__":
+    main()
